753ff6d59d974bdda7f32ecf241b86330d9a61f9
Haneji Yuto <49736402+Y-Haneji@users.noreply.github.com>
2021-12-09 16:47:46 +0900

    dropout層を追加したコンテスト予想

--- contest.py
+++ contest.py
@@ -1,11 +1,11 @@
-from ex4 import Model
+from ex_advanced import Model
 import mnist
 import numpy as np
 
 if __name__ == '__main__': 
   X = np.loadtxt("le4MNIST_X.txt")
 
-  model = Model()
-  model.load_model('0001')
+  model = Model('inference')
+  model.load_model('0002')
   Y = model.predict(X)
   np.savetxt('predict.txt', Y, fmt='%d')
