47fbe5df619b0f38beed501846c72f24e5d7e5e3
Haneji Yuto <49736402+Y-Haneji@users.noreply.github.com>
2021-12-24 13:21:21 +0900

    tensorflow の sequential ライクなインターフェースに変更

--- ex_advanced.py
+++ ex_advanced.py
@@ -10,17 +10,155 @@ from logger import Logger
 
 random.seed(71)
 
-class Model():
-  def __init__(self, mode = 'train', dropout: float = 0.0) -> None:
+class SGD:
+  def __init__(self, lr: float=0.01):
+    self.lr = lr
+  
+  def update(self, grad_w, grad_b):
+    dw = -self.lr*grad_w
+    db = -self.lr*grad_b
+    return dw, db
+
+
+def get_opt(opt: str, *args, **kwds):
+  if opt == 'SGD':
+    optimizer = SGD(*args, **kwds)
+  else:
+    raise ValueError(f'{opt} is not implemented.')
+
+  return optimizer
+
+class Layer:
+  def __init__(self, *args, **kwds):
+    pass
+
+  def forward(self, x, *args, **kwds):
+    self.x = x
+    self.y = x
+    return self.y
+
+  def backward(self, grad, *args, **kwds):
+    self.grad_x = grad
+    return self.grad_x
+
+  def update(self, *args, **kwds):
+    pass
+
+  def get_weight(self, *args, **kwds) -> tuple:
+    return ()
+
+class Input(Layer):
+  def __init__(self, output_shape: tuple):
+    self.output_shape = output_shape
+  
+  def forward(self, x, batch_size=100, mode='train'):
+    self.x = x
+    self.y = x.reshape(((batch_size,) + self.output_shape)).T
+    return self.y
+
+
+class Dense(Layer):
+  def __init__(self, units: int, input_shape: tuple, opt: str='SGD', opt_kwds: dict={}):
+    self.units = units
+    self.input_shape = input_shape
+    input_size = 1
+    for size in input_shape:
+      input_size *= size
+
+    self.w = random.normal(loc=0, scale=np.sqrt(1/input_size), size=input_size*units).reshape(units, input_size)
+    self.b = random.normal(loc=0, scale=np.sqrt(1/input_size), size=units)
+    self.opt = get_opt(opt, **opt_kwds)
+
+  def forward(self, x, batch_size=100, mode='train'): # ミニバッチに対応
+    # TODO: 画像入力に対応
+    self.x = x
+    self.y = self.w@x + self.b.reshape(-1, 1)
+    return self.y
+
+  def backward(self, grad):
+    self.grad_x = self.w.T@grad
+    self.grad_w = grad@self.x.T
+    self.grad_b = np.sum(grad, axis=1)
+    return self.grad_x
+
+  def update(self):
+    dw, db = self.opt.update(self.grad_w, self.grad_b)
+    self.w += dw
+    self.b += db
+
+  def get_weight(self) -> tuple:
+    return self.w, self.b
+
+class Dropout(Layer):
+  def __init__(self, dropout: float=0.1):
+    self.dropout = dropout
+  
+  def forward(self, x, batch_size=100, mode='train'):
+    self.x = x
+    if mode == 'train':
+      dropped = np.zeros(x.shape)
+      num_drop = math.floor(x.shape[0]*self.dropout)
+      nodes_dropped = []
+      for batch in range(x.shape[1]):
+        node_dropped = random.choice(np.arange(batch*x.shape[0], (batch+1)*x.shape[0]), num_drop, replace=False)
+        nodes_dropped.extend(node_dropped)
+
+      np.put(dropped, nodes_dropped, 1)
+      self.y = x
+      np.place(self.y, dropped, 0)
+      self.flag_not_dropped = np.where(dropped, 0, 1)
+      return self.y
+
+    if mode == 'inference':
+      return x*(1-self.dropout)
+
+  def backward(self, grad):
+    self.grad_x = grad*self.flag_not_dropped
+    return self.grad_x
+
+
+class Sigmoid(Layer):
+  def __init__(self):
+    pass
+
+  def forward(self, x, batch_size=100, mode='train'):
+    sigmoid_range = 34.538776394910684
+    x = np.clip(x, -sigmoid_range, sigmoid_range)
+    self.x = x
+    self.y = 1/(1.0+np.exp(-x))
+    return self.y
+
+  def backward(self, grad):
+    self.grad_x = grad*(1-self.y)*self.y
+    return self.grad_x
+
+
+class Softmax(Layer):
+  def __init__(self):
+    pass
+
+  def forward(self, x, batch_size=100, mode='train'):
+    self.batch_size = batch_size
+    self.x = x
+    self.y = np.exp(x-np.max(x, axis=0)) / (np.sum(np.exp(x-np.max(x, axis=0)), axis=0))
+    return self.y
+
+  def backward(self, true_y):
+    grad_x = (self.y - true_y.T)/self.batch_size
+    return grad_x
+
+
+class Model:
+  def __init__(self, mode = 'train') -> None:
     # モデルのアーキテクチャを作成
-    self.units = 32
     self.batch_size = 100
-    self.w1, self.b1 = random.normal(loc=0, scale=np.sqrt(1/784), size=784*self.units).reshape(self.units, 784), random.normal(loc=0, scale=np.sqrt(1/784), size=self.units)
-    self.w2, self.b2 = random.normal(loc=0, scale=np.sqrt(1/self.units), size=self.units*10).reshape(10, self.units), random.normal(loc=0, scale=np.sqrt(1/self.units), size=10)
     if mode not in ['train', 'inference']:
       raise ValueError('mode is train or inference.')
     self.mode = mode
-    self.drop_prob = dropout
+    self.layers = []
+
+  def add(self, layer: Layer):
+    self.layers.append(layer)
 
   def preprocessing(self, train_x, train_y):
     '''学習用セットからバッチを作成する'''
@@ -30,41 +168,6 @@ class Model():
     tr_y = np.zeros((len(l), len(l[0])))
     tr_y[:] = l
     return tr_x, tr_y # tr_x is (bs, 28, 28) tr_y is one-hot-encoding (bs, 10)
-
-  def input_layer(self, im):
-    return im.reshape((self.batch_size, 784)).T
-
-  def dense_layer(self, input_vec, weight, bias): # ミニバッチに対応
-    return weight@input_vec+bias.reshape(-1, 1)
-
-  def dropout(self, input_vec):
-    if self.mode == 'train':
-      dropped = np.zeros(input_vec.shape)
-      num_drop = math.floor(input_vec.shape[0]*self.drop_prob)
-      nodes_dropped = []
-      for batch in range(input_vec.shape[1]):
-        node_dropped = random.choice(np.arange(batch*input_vec.shape[0], (batch+1)*input_vec.shape[0]), num_drop, replace=False)
-        nodes_dropped.extend(node_dropped)
-      np.put(dropped, nodes_dropped, 1)
-      np.place(input_vec, dropped, 0)
-      alived = np.where(dropped, 0, 1)
-      return input_vec, alived
-    elif self.mode == 'inference':
-      return input_vec*(1-self.drop_prob)
-    else:
-      raise ValueError('mode is train or inference.')
-
-  def dense_layer1(self, input_vec):
-    return self.dense_layer(input_vec, self.w1, self.b1)
-
-  def sigmoid(self, input_vec):
-    return 1/(1+np.exp(-input_vec))
-
-  def dense_layer2(self, input_vec):
-    return self.dense_layer(input_vec, self.w2, self.b2)
-
-  def softmax(self, input_vec): # ミニバッチに対応
-    return np.exp(input_vec-np.max(input_vec, axis=0)) / (np.sum(np.exp(input_vec-np.max(input_vec, axis=0)), axis=0))
 
   def postprocessing(self, input_vec): # ミニバッチに対応
     return np.argmax(input_vec, axis=0)
@@ -78,32 +181,29 @@ class Model():
     return np.count_nonzero(true_y == pred_y) / true_y.shape[0]
 
   def train_batch(self, tr_x, tr_y, lr):
-    x = self.input_layer(tr_x)
-    t = self.dense_layer1(x)
-    y = self.sigmoid(t)
-    y_dash, alived = self.dropout(y)
-    a = self.dense_layer2(y_dash)
-    y2 = self.softmax(a)
-    e_n = self.cross_entropy(tr_y.T, y2)
+    flag_input = True
+    for layer in self.layers:
+      if flag_input == True:
+        x = tr_x
+        flag_input = False
+      x = layer.forward(x, self.batch_size, self.mode)
 
-    # 以下微分の形を行*列で示す
-    diff_e_n_with_a = (y2 - tr_y.T)/self.batch_size # クラス数*bs
-    diff_e_n_with_y_dash = self.w2.T @ diff_e_n_with_a # units*bs
-    diff_e_n_with_w2 = diff_e_n_with_a @ y_dash.T # クラス数*units
-    diff_e_n_with_b2 = np.sum(diff_e_n_with_a, axis=1) # クラス数次元ベクトル
-    diff_e_n_with_y = diff_e_n_with_y_dash * alived
-    diff_e_n_with_t = diff_e_n_with_y*(1-y)*y # units*bs
-    diff_e_n_with_x = self.w1.T @ diff_e_n_with_t # 784*bs
-    diff_e_n_with_w1 = diff_e_n_with_t @ x.T # units*784
-    diff_e_n_with_b1 = np.sum(diff_e_n_with_t, axis=1) # units数次元ベクトル
+    entropy = self.cross_entropy(tr_y, x.T)
 
-    # 重みの更新
-    self.w1 = self.w1 - lr*diff_e_n_with_w1
-    self.b1 = self.b1 - lr*diff_e_n_with_b1
-    self.w2 = self.w2 - lr*diff_e_n_with_w2
-    self.b2 = self.b2 - lr*diff_e_n_with_b2
+    flag_output = True
+    for layer in reversed(self.layers):
+      if flag_output == True:
+        grad = tr_y
+        flag_output = False
+      grad = layer.backward(grad)
 
-    return (self.w1, self.b1, self.w2, self.b2), e_n
+    for layer in self.layers:
+      layer.update()
+
+    weights = ()
+    for layer in self.layers:
+      weights = weights + layer.get_weight()
+    return weights, entropy
   
   def train(self, train_x, train_y, epochs: int = 10, lr: float = 0.01):
     '''
@@ -115,9 +215,12 @@ class Model():
     self.batch_size = 100
     history = []
     for i in tqdm(range(epochs)):
+      entropies = []
       for j in range(60000//self.batch_size):
         tr_x, tr_y = self.preprocessing(train_x, train_y)
         model, entropy = self.train_batch(tr_x, tr_y, lr)
+        entropies.append(entropy)
+      entropy = sum(entropies)/len(entropies)
       print(f'Epoch {i+1} end! Cross entroppy is {entropy}.')
       history.append((model,entropy))
     return history
@@ -125,22 +228,21 @@ class Model():
   def predict(self, test_x):
     self.mode = 'inference'
     self.batch_size = 1 # 推論時はバッチ処理を行わない
-    pred_y = [self.postprocessing(self.softmax(self.dense_layer2(self.dropout(self.sigmoid(self.dense_layer1(self.input_layer(np.expand_dims(test_i, axis=0)))))))) for test_i in (test_x)]
-    # pred_y = []
-    # for test_i in tqdm(test_x):
-    #   x = self.input_layer(np.expand_dims(test_i, axis=0)) # バッチサイズ1のミニバッチを作成
-    #   t = self.dense_layer1(x)
-    #   y = self.sigmoid(t)
-    #   a = self.dense_layer2(y)
-    #   y2 = self.softmax(a)
-    #   pred_y_i = self.postprocessing(y2)
-    #   print(pred_y_i)
-    #   pred_y.append(pred_y_i)
+    pred_y = []
+    for test_i in (test_x):
+      flag_input = True
+      for layer in self.layers:
+        if flag_input == True:
+          x = test_i
+          flag_input = False
+        x = layer.forward(x, self.batch_size, self.mode)
+      pred_y.append(self.postprocessing(self.layers[-1].y))
+
     pred_y = np.array(pred_y).flatten()
     return pred_y
 
   def load_best(self, history):
-    self.w1, self.b1, self.w2, self.b2 = history[np.nanargmin(history[1])][0]
+    self.w1, self.b1, self.w2, self.b2 = history[np.nanargmin(list(zip(*history))[1])][0]
 
   def save_model(self, name):
     np.savez(f'model/{name}', w1=self.w1, b1=self.b1, w2=self.w2, b2=self.b2)
@@ -156,11 +258,19 @@ if __name__ == '__main__':
   logger = Logger()
   train_x = mnist.download_and_parse_mnist_file('train-images-idx3-ubyte.gz')
   train_y = mnist.download_and_parse_mnist_file("train-labels-idx1-ubyte.gz")
-  model = Model(mode ='train', dropout=0.1)
+
+  model = Model(mode ='train')
+  model.add(Input((28*28,)))
+  model.add(Dense(32, (28*28,), 'SGD', {'lr': 0.01}))
+  model.add(Sigmoid())
+  model.add(Dropout(dropout=0.1))
+  model.add(Dense(10, (32,), 'SGD', {'lr': 0.01}))
+  model.add(Softmax())
+
+  # model.load_model('0001')
   history = model.train(train_x, train_y)
   model.load_best(history)
   model.save_model(run_name)
-  # model.load_model('0001')
 
   test_x = mnist.download_and_parse_mnist_file('t10k-images-idx3-ubyte.gz')
   test_y = mnist.download_and_parse_mnist_file('t10k-labels-idx1-ubyte.gz')
@@ -170,6 +280,6 @@ if __name__ == '__main__':
   print('please input log message about this run.')
   logger.info(input())
   logger.info(f'this model is {run_name}')
-  logger.info(f'best entropy for train is {min(history, key=lambda p: p[1])[1]}.')
+  logger.info(f'best entropy for train is {np.nanmin(list(zip(*history))[1])}.')
   logger.info(f'accuracy score for test is {model.accuracy(test_y, pred_y)}')
   logger.info('')
