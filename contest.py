1ceab6ce98ec4878fc5c84238fd0717a0060ef16
Haneji Yuto <49736402+Y-Haneji@users.noreply.github.com>
2022-01-05 18:30:30 +0900

    発展課題A全てを終えたコンテスト

--- contest.py
+++ contest.py
@@ -1,11 +1,20 @@
-from ex_advanced import Model
-import mnist
+from ex_advanced import *
 import numpy as np
 
 if __name__ == '__main__': 
   X = np.loadtxt("le4MNIST_X.txt")
+  X = X.astype('float32')/255.0  # 正規化
 
   model = Model('inference')
-  model.load_model('0002')
+  model.add(Input((28, 28)))
+  model.add(Conv(input_shape=(28, 28), filter_shape=(3, 3), filter_num=32, opt='Adam', opt_kwds={}))
+  model.add(ReLU())
+  model.add(Pooling(input_shape=(28, 28), channel=32, pool_shape=(2, 2)))
+  model.add(Flatten())
+  model.add(Dropout(dropout=0.4))
+  model.add(Dense(10, (32,28,28), opt='Adam', opt_kwds={}))
+  model.add(Softmax())
+  model.load_model('0012')
   Y = model.predict(X)
+  print(Y)
   np.savetxt('predict.txt', Y, fmt='%d')
